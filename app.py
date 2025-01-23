cf8128f20e83ed6232830f691fe44a6aff52a51d
theAtieno <mjacinta073@gmail.com>
2025-01-23 21:18:14 +0300

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import random
-game = ("Rock", "Paper", "Scissors")
-my_choice = input("Choose Rock, Paper or Scissors:")
-computer_choice = random.choice(game)
-print("You chose:", my_choice)
-print("Computer chose:", computer_choice)
+def play_game(rounds): #function that declares the number of rounds
+    game = ("Rock", "Paper", "Scissors")
+    my_score= 0 #variable to track the scores of the player
+    computer_score= 0 #variable to track the scores of the computer
+print("Game starts! First to", round, "rounds will earn points, and the one with the most points wins the game.")
+
+round_count =0
+while round
+# my_choice = input("Choose Rock, Paper or Scissors:")
+# computer_choice = random.choice(game)
+# print("You chose:", my_choice)
+# print("Computer chose:", computer_choice)

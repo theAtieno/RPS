8f18b113b1a034a020f93b428fd9c8d8fd5ef7b3
theAtieno <mjacinta073@gmail.com>
2025-01-23 18:57:51 +0300

    Utilise the Random Python Library to allow the computer to randomly choose rock, paper, or scissors in each round Allow the user to now input their choice and compare it directly with the computer’s choice. You can then declare the winner of the round based on the rules of the game

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-#code
+import random
+game = ("Rock", "Paper", "Scissors")
+my_choice = input("Choose Rock, Paper or Scissors:")
+computer_choice = random.choice(game)
+print("You chose:", my_choice)
+print("Computer chose:", computer_choice)

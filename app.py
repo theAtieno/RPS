13dad845f8ef41ce4449787a223bc4ff3af4804d
theAtieno <mjacinta073@gmail.com>
2025-01-23 21:43:25 +0300

    Part 1 of Week 1 Challenge

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
+#Utilise the Random Python Library to allow the computer to randomly choose rock, paper, or scissors in each round
 import random
-def play_game(rounds): #function that declares the number of rounds
-    game = ("Rock", "Paper", "Scissors")
-    my_score= 0 #variable to track the scores of the player
-    computer_score= 0 #variable to track the scores of the computer
-print("Game starts! First to", round, "rounds will earn points, and the one with the most points wins the game.")
-
-round_count =0
-while round
-# my_choice = input("Choose Rock, Paper or Scissors:")
-# computer_choice = random.choice(game)
-# print("You chose:", my_choice)
-# print("Computer chose:", computer_choice)
+choices = ("rock", "paper", "scissors")
+computer_choice = random.choice(choices)
+for _ in range(3):
+    print(computer_choice)
